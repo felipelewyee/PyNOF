cc6036895dd4b09580009a2357675730a5a17d8d
Juan Felipe Huan Lew Yee <felipe.lew.yee@gmail.com>
2021-09-17 11:48:34 -0500

    Se agrega version en gpu

--- build/lib/pynof/guess.py
+++ build/lib/pynof/guess.py
@@ -1,18 +1,18 @@
 import numpy as np
 
-def read_C(title = "pydonof"):
+def read_C(title = "pynof"):
     C = np.load(title+"_C.npy")
     return C
 
-def read_gamma(title = "pydonof"):
+def read_gamma(title = "pynof"):
     gamma = np.load(title+"_gamma.npy")
     return gamma
 
-def read_fmiug0(title = "pydonof"):
+def read_fmiug0(title = "pynof"):
     fmiug0 = np.load(title+"_fmiug0.npy")
     return fmiug0
 
-def read_all(title = "pydonof"):
+def read_all(title = "pynof"):
     C = read_C(title)
     gamma = read_gamma(title)
     fmiug0 = read_fmiug0(title)
